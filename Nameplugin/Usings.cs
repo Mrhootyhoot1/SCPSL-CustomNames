baca74a92f7ea98922368a71fd889e6e5021f3b6
Mrhootyhoot1 <liamchristopher1026@gmail.com>
2026-01-05 15:50:00 -0500

    Bump Exiled version to 9.12.2

--- Nameplugin/Usings.cs
+++
@@ -1,7 +0,0 @@
-﻿using URandom = UnityEngine.Random;
-using UObject = UnityEngine.Object;
-using ExPlayer = Exiled.API.Features.Player;
-
-using ServerHandlers = Exiled.Events.Handlers.Server;
-using Scp914Handlers = Exiled.Events.Handlers.Scp914;
-using PlayerHandlers = Exiled.Events.Handlers.Player;
